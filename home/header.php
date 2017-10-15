f7e17a68698c8fa8b01dc6c8bf3e403a1f01b62e
hu970804 <812510003@qq.com>
2017-10-15 11:36:04 +0800

    重写header

--- home/header.php
+++ home/header.php
@@ -1,9 +1,41 @@
-<div id="container">
-	<div class="header">
-		<div class="logo"><a href="index.html">微益捐</a></div>
-		<div class="login">
-			<div class="signIn"><a href="./sign_up.php">登录</a></div>
-			<div class="signUp"><a href="./sign_in.php">注册</a></div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+	<link rel="stylesheet" href="./css/index.css">
+	<script src="./js/js.js"></script>
+	<title>Document</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="logo">
+				<a href="index.html">
+					<img src="./img/logo.png" alt="" /> </a>
+			</div>
+			<span class="menu">
+				<img src="./img/icon.png" alt="" />
+			</span>
+			<div class="clear"> </div>
+			<div class="navg">
+				<ul class="res">
+					<li>
+						<a href="index.html">首页</a>
+					</li>
+					<li>
+						<a href="./sign_in.php">登陆</a>
+					</li>
+					<li>
+						<a href="./sign_up.php">注册</a>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
 		</div>
 	</div>
-</div>
+	
