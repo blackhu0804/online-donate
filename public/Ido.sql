70f96b1368baaca0ca4d5ae3a8820ec8d2e8f1fb
AmyZ666 <564849248@qq.com>
2017-10-18 19:04:51 +0800

    数据库表更新

--- public/Ido.sql
+++ public/Ido.sql
@@ -3,34 +3,63 @@ create database Ido;
 -- 切换数据库
 use Ido;
 
--- 1.user
+-- 1.user用户表
 create table user(
 	id int not null auto_increment primary key,
 	username varchar(50) not null,
 	emaile varchar(50) not null,
 	password varchar(50) not null,
+	integration int,
 	isadmin tinyint not null
 );
 
--- 2.giftClass
+-- 2.giftClass项目
 create table giftClass(
 	id int not null auto_increment primary key,
 	name varchar(100) not null,
-	expl text,
-	img varchar(100) not null,
+	img varchar(1000) not null,
+	expl text not null,
 	user_num int,
-	sum_money float,
-	organization varchar(100),
-	isend tinyint not null,
-	time int
+	use_money int not null,
+	sum_money int,
+	start_time int not null,
+	end_time int not null,
+	isend tinyint not null
 );
 
--- 3.giftInfo
+-- 3.giftInfo用户善心表
 create table giftInfo(
 	id int not null auto_increment primary key,
-	money float not null,
-	content text,
 	giftClass_id int not null,
-	user_id int not null,
-	time int
+	money int not null,
+	time int,
+	content text,
+	user_id int not null
+);
+
+-- 4.giftMarch项目进展报告
+create table giftMarch(
+	id int not null auto_increment primary key,
+	giftClass_id int not null,
+	time int,
+	img varchar(1000),
+	content text
+);
+
+-- 5.sum_history历史表
+create table sum_history(
+	id int not null auto_increment primary key,
+	sum_money int,
+	sum_user int
+);
+
+-- 6.certification实名认证表
+
+create table certification(
+	id int not null auto_increment primary key,
+	name varchar(100) not null,
+	ID_card varchar(100) not null,
+	myPhone varchar(100) not null,
+	user_id varchar(100) not null,
+	bank_ID varchar(100) not null
 );
---
+++ public/ceshi.sql
@@ -0,0 +1,8 @@
+create table certification(
+	id int not null auto_increment primary key,
+	name varchar(100) not null,
+	ID_card varchar(100) not null,
+	myPhone varchar(100) not null,
+	user_id varchar(100) not null,
+	bank_ID varchar(100) not null
+);
