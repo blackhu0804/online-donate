7814cd26f12f6c0942e63d127b3ce555746241c2
hu970804 <812510003@qq.com>
2017-10-25 11:34:06 +0800

    add gotop

--- home/footer.php
+++ home/footer.php
@@ -1,4 +1,7 @@
-<div class="footer" >
+  <div class="goTop">
+    <img src="./img/goTop.gif" alt="">
+  </div>
+  <div class="footer" >
 		<p class="footer-desc">
       <a href="/">关于我们</a>
       |
@@ -8,3 +11,22 @@
     </p>
     <p>&copy;<script>document.write('2016-' + new Date().getFullYear())</script> 微益捐版权所有</p>
 	</div>
+
+
+  <script src="./js/jquery-3.2.1.min.js"></script>
+  <script>
+    $(function(){
+      $(window).scroll(function(){
+        var s = $(window).scrollTop();
+        if( s > 400){
+          $(".goTop").fadeIn(500);
+        }else{
+          $(".goTop").fadeOut(500);
+        };
+      })
+      
+      $('.goTop').click(function(){
+        $('html , body').animate({scrollTop: 0},'slow');
+      })
+    })
+  </script>
